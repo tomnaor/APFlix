68a7ae8ed4b9008a0ed2f646005538804b9b214f
tomnaor <tomnaor78@gmail.com>
2023-10-03 22:43:01 +0300

    add favicon and change title

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const ApiAxiosInstance = axios.create({
-  baseURL: `http://localhost:3000/`,
+  baseURL: ``,
 });
--- frontend/src/components/WelcomeHeader/WelcomeHeader.tsx
+++ frontend/src/components/WelcomeHeader/WelcomeHeader.tsx
@@ -1,9 +1,14 @@
+import LogoSvg from "../../../public/logo.svg";
+
 export const WelcomeHeader = () => {
   return (
     <div className="flex w-full pb-2 border-b items-center justify-between">
-      <h1 className="scroll-m-20 text-xl font-semibold tracking-tight transition-colors first:mt-0 text-center">
-        APFlix
-      </h1>
+      <div className="flex gap-1 items-center">
+        <img src={LogoSvg} alt="logo" />
+        <h1 className="scroll-m-20 text-xl font-semibold tracking-tight transition-colors first:mt-0 text-center">
+          APFlix
+        </h1>
+      </div>
       <p className="text-sm text-center">Your movies recommendation platform</p>
     </div>
   );
